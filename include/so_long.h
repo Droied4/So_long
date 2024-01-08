5743643848c328c763f01ac1ab0a3d8fb4ad9483
Carmen Ordoñez <deiord44santiagoov@gmail.com>
2024-01-08 04:13:41 +0100

    green rectangle

--- include/so_long.h
+++ include/so_long.h
@@ -6,15 +6,38 @@
 /*   By: carmeno <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 22:46:45 by carmeno           #+#    #+#             */
-/*   Updated: 2024/01/07 00:21:06 by carmeno          ###   ########.fr       */
+/*   Updated: 2024/01/08 03:30:37 by carmeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 # include "../library/libft/libft.h"
-# include "../library/printf/ft_printf.h"
 # include "../library/minilibx/mlx.h"
+# include "../library/printf/ft_printf.h"
 
+/*Datos para las imagenes*/
+typedef struct s_data_img
+{
+	void		*img;
+	char		*addr;
+	int		bpp;
+	int		l_line;
+	int		endian;
+}			t_data_img;
+
+typedef struct s_color
+{
+	int		red;
+	int		blue;
+	int		yellow;
+	int		cyan;
+	int		brown;
+	int		magenta;
+	int		orange;
+	int		green;
+	int		black;
+	int		white;
+}			t_color;
 
 #endif
--- src/so_long.c
+++ src/so_long.c
@@ -6,26 +6,44 @@
 /*   By: carmeno <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 22:46:12 by carmeno           #+#    #+#             */
-/*   Updated: 2024/01/07 02:30:03 by carmeno          ###   ########.fr       */
+/*   Updated: 2024/01/08 04:12:58 by carmeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int main (void)
+void	my_pixel_put(t_data_img *data, int x, int y, int color)
 {
-	void	*mlx;
-	void	*mlx_win;
-	
-	ft_putstr_fd("libft ok\n", 1);
-	ft_printf("printf %s\n","ok");
+	char *dst;
+
+	dst = data->addr + (y * data->l_line + x * (data->bpp / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	main(void)
+{
+	void		*mlx;
+	void		*mlx_win;
+	t_data_img	img;
 
 	mlx = mlx_init();
 	if (!mlx)
 		exit(1);
-	mlx_win = mlx_new_window(mlx, 300, 300, "santito");
+	img.img = mlx_new_image(mlx, 500, 500);
+	if (!img.img)
+		exit(1);
+	mlx_win = mlx_new_window(mlx, 500, 500, "-Droied-");
 	if (!mlx_win)
 		exit(1);
+	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.l_line, &img.endian);
+	int y = 99;
+	while (++y < 400)
+	{
+		int x = 99;
+		while (++x < 400)
+			my_pixel_put(&img, x, y, 0x0000FF00);
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 	return (0);
 }
